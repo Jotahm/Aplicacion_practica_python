1749d08bc0d2018a5ce5417070c0418f25879a90
Jotahm <jhmarcianes@gmail.com>
2021-01-15 18:40:07 +0100

    Calculadora sumatoria
    Ejercicio de aplicación práctica: realizando una calculadora simple pero eficaz: una sumatoria

---
+++ calculadora_basica.py
@@ -0,0 +1,14 @@
+# CALCULADORA BÁSICA - PROYECTO N1
+
+# Declaramos las variables
+# Utilizamos input para que se guarde el dato introducido
+num1 = input("Introduce un número: ")
+num2 = input ("Introduce otro número: ")
+
+# Realizamos la suma y guardamos resultado
+res = float(num1) + float(num2) # Poniendo float permitimos operar con decimales ; en caso contrario, está concatenando dos cadenas
+
+# Imprimimos el resultado y un mensaje de despedida
+
+print ("La suma es:", res)
+print ("Gracias por usarme")
