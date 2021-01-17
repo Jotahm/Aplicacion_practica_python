0bfe2fb68a10d9f6734e9706df28234ffa28e42e
Jotahm <jhmarcianes@gmail.com>
2021-01-17 22:30:59 +0100

    Calculadora aritmética

---
+++ 1.1_calculadora_aritmetica.py
@@ -0,0 +1,18 @@
+# CALCULADORA
+
+# Declaramos las variables
+# Utilizamos input para que se guarde el dato introducido
+num1 = input("Introduce un número: ")
+signo = input ("Introduce signo aritmético: ")
+num2 = input ("Introduce otro número: ")
+
+if signo = "+":
+    res = num1 + num2
+
+# Realizamos la suma y guardamos resultado
+res = float(num1) + float(num2) # Poniendo float permitimos operar con decimales ; en caso contrario, está concatenando dos cadenas
+
+# Imprimimos el resultado y un mensaje de despedida
+
+print ("La suma es:", res)
+print ("Gracias por usarme")
