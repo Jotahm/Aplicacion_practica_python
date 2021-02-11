f44e14f694667837b42e8a1404d270af9e6f853c
Josué Hormeño <76787941+Jotahm@users.noreply.github.com>
2021-02-11 21:56:26 +0100

    Create 2_ejemplo_bucle_definido.py

---
+++ 2_ejemplo_bucle_definido.py
@@ -0,0 +1,8 @@
+# Ejemplo bucle definido
+
+colores =["rojo", "rosa", "verde", "amarillo"]
+
+for color in colores:
+    print ("Mi color favorito es ", color)
+    
+print ("Me gustan todos los colores")
